03b6025cf62d54c3a6fc86ccd21c5271f47604e8
Zerodayyz <108452509+Zerodayyz@users.noreply.github.com>
2022-06-30 00:56:04 -0700

    Add files via upload

---
+++ dumped_offsets.cpp
@@ -0,0 +1,39 @@
+	vtables:
+	GetPlayerViewPoint = 0x7A8
+	SetControlRotation = 0x718
+	GetCameraLocation = 0x7B8          
+	GetCameraRotation = 0x7B0         
+	Process Event = 0x4D               
+	GetFOVAngle = 0x770                            
+	Post Render = 0x6E
+
+	Offsets:
+	UWORLD 0xcbf58f8
+	GOBJECT 0xca46f08
+	GNAMES 0xcc49540
+	uintptr_t OwningGameInstance = 0x1A8;
+	uintptr_t DisplayName = 0x90;
+	uintptr_t Tier = 0x74;
+	uintptr_t LastFireTimeVerified = 0xa74;
+	uintptr_t LastFireAbilityTime = 0xd60;
+	uintptr_t CurrentWeapon = 0x790;
+	uintptr_t bADSWhileNotOnGround = 0x4805;
+	uintptr_t WeaponData = 0x3d8;
+	uintptr_t LastFireTime = 0xa70;
+	uintptr_t Levels = 0x160;
+	uintptr_t LocalPlayers = 0x38;
+	uintptr_t PlayerController = 0x30;
+	uintptr_t PlayerCameraManager = 0x328;
+	uintptr_t AcknowledgedPawn = 0x310;
+	uintptr_t PrimaryPickupItemEntry = 0x2f8;
+	uintptr_t ItemDefinition = 0x18;
+	uintptr_t PersistentLevel = 0x30;
+	uintptr_t FireStartLoc = 0x9f0;
+	uintptr_t RelativeLocation = 0x128;
+	uintptr_t RelativeRotation = 0x140;
+	uintptr_t PlayerState = 0x290;
+	uintptr_t AActors = 0x98;
+	uintptr_t ActorCount = 0xA0;
+	uintptr_t RootComponent = 0x188;
+	uintptr_t Mesh = 0x2f0;
+	uintptr_t TeamIndex = 0x1070;
---
+++ dumped_sigs.cpp
@@ -0,0 +1,8 @@
+Uworld = 48 89 05 ? ? ? ? 48 8B 4B 78 
+FreeFN = 48 85 C9 0F 84 ? ? ? ? 53 48 83 EC 20 48 89 7C 24 30 48 8B D9 48 8B 3D ? ? ? ? 48 85 FF 0F 84 ? ? ? ? 48 8B 07 4C 8B 40 30 48 8D 05 ? ? ? ? 4C 3B C0 
+ProjectWorldToScreen = E8 ? ? ? ? 4C 8D 5C 24 ? 41 88 06 49 8B 5B 20 49 8B 73 30 49 8B 7B 38 
+LineOfSight = 48 89 5C 24 ? 48 89 74 24 ? 55 57 41 56 48 8B EC 48 83 EC 60 33 FF 4D 8B F0 48 89 7D 28 48 8B DA 48 8B F1 E8 ? ? ? ? 48 8B CB 48 39 7B 20 74 0F 48 8B 53 18 4C 8D 45 28 E8
+GetNameByIndex = 48 89 5C 24 ? 48 89 74 24 ? 57 48 81 EC ? ? ? ? 48 8B 05 ? ? ? ? 48 33 C4 48 89 84 24 ? ? ? ? 8B 01 48 8B F2 // returns 0
+BoneMatrix = E8 ? ? ? ? 0F 10 40 68
+Gobjects = 48 8B 05 ? ? ? ? 48 8B 0C C8 48 8B 04 D1
+FindObject = E8 ? ? ? ? 40 80 EF 03
